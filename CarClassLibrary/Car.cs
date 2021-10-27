4caa4ab12f1f8a26ef92739e4da0583c13fad5e4
Administrator <Administrator@DESKTOP-QB4O65F>
2021-10-27 22:02:06 +0500

    Add project files.

---
+++ CarClassLibrary/Car.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClassLibrary
+{
+    class Car
+    {
+        // Properties
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public decimal Price { get; set; }
+
+        // Constructors
+
+        public Car()
+        {
+            this.Make = "---";
+            this.Model = "---";
+            this.Price = 0.00M;
+        }
+        public Car(string Make, string Model, decimal Price)
+        {
+            this.Make = Make;
+            this.Model = Model;
+            this.Price = Price;
+        }
+    }
+}
